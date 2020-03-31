d9484fb533a668436bc15f64435c94146c9e5ac9
王恺 <kai.wang@QXIT-PC-000970.local>
2020-03-31 11:40:02 +0800

    kg_game update

--- reinforcement_learning/kb_game.py
+++ reinforcement_learning/kb_game.py
@@ -4,56 +4,52 @@ import matplotlib.pyplot as plt
 
 class KB_Game:
     def __init__(self, *args, **kwargs):
-        self.q = np.array([0.0, 0.0, 0.0])
+        self.rewards = np.array([0.0, 0.0, 0.0])
         self.action_counts = np.array([0, 0, 0])
-        self.current_cumulative_rewards = 0.0
-        self.actions = [1, 2, 3]
+        self.actions = [0, 1, 2]
         self.counts = 0
         self.counts_history = []
+        self.current_cumulative_reward = 0.0
         self.cumulative_rewards_history = []
-        self.a = 1
-        self.reward = 0
+        self.current_action = 1
+        self.current_reward = 0.0
 
-    def step(self, a):
-        r = 0
-        if a == 1:
-            r = np.random.normal(1, 1)
-        if a == 2:
-            r = np.random.normal(2, 1)
-        if a == 3:
-            r = np.random.normal(1.5, 1)
+    def step(self, action):
+        reward = 0
+        if action == 0:
+            reward = np.random.normal(1, 1)
+        if action == 1:
+            reward = np.random.normal(2, 1)
+        if action == 2:
+            reward = np.random.normal(1.5, 1)
 
-        return r
+        return reward
 
     def choose_action(self, policy, **kwargs):
         action = 0
         if policy == 'e_greedy':
             if np.random.random() < kwargs['epsilon']:
-                action = np.random.randint(1, 4)
+                action = np.random.randint(0, 3)
             else:
-                action = np.argmax(self.q) + 1
+                action = np.argmax(self.rewards)
 
         if policy == 'ucb':
             c_ratio = kwargs['c_ratio']
             if 0 in self.action_counts:
-                action = np.where(self.action_counts == 0)[0][0] + 1
+                action = np.where(self.action_counts == 0)[0][0]
             else:
-                value = self.q + c_ratio * np.sqrt(np.log(self.counts) / self.action_counts)
-                action = np.argmax(value) + 1
+                values = self.rewards + c_ratio * np.sqrt(np.log(self.counts) / self.action_counts)
+                action = np.argmax(values)
 
         if policy == 'boltzmann':
             tau = kwargs['temperature']
-            p = np.exp(self.q / tau) / (np.sum(np.exp(self.q / tau)))
-            action = np.random.choice([1, 2, 3], p=p.ravel())
+            p = np.exp(self.rewards / tau) / (np.sum(np.exp(self.rewards / tau)))
+            action = np.random.choice(self.actions, p=p.ravel())
 
         return action
 
-    def train(self, play_total, policy, **kwargs):
-        reward_1 = []
-        reward_2 = []
-        reward_3 = []
-
-        for i in range(play_total):
+    def train(self, play_times, policy, **kwargs):
+        for i in range(play_times):
             action = 0
             if policy == 'e_greedy':
                 action = self.choose_action(policy, epsilon=kwargs['epsilon'])
@@ -62,53 +58,47 @@ class KB_Game:
             if policy == 'boltzmann':
                 action = self.choose_action(policy, temperature=kwargs['temperature'])
 
-            self.a = action
-            self.r = self.step(self.a)
+            self.current_action = action
+            self.current_reward = self.step(self.current_action)
             self.counts += 1
-            self.q[self.a - 1] = (self.q[self.a - 1] * self.action_counts[self.a - 1] + self.r) / (
-                    self.action_counts[self.a - 1] + 1)
+            self.rewards[self.current_action] = self.rewards[self.current_action] + \
+                                                (self.current_reward - self.rewards[self.current_action]) \
+                                                / (self.action_counts[self.current_action] + 1)
 
-            self.action_counts[self.a - 1] += 1
-
-            reward_1.append([self.q[0]])
-            reward_2.append([self.q[1]])
-            reward_3.append([self.q[2]])
-
-            self.current_cumulative_rewards += self.r
-
-            self.cumulative_rewards_history.append(self.current_cumulative_rewards)
+            self.action_counts[self.current_action] += 1
+            self.current_cumulative_reward += self.current_reward
+            self.cumulative_rewards_history.append(self.current_cumulative_reward)
             self.counts_history.append(i)
 
     def reset(self):
-        self.q = np.array([0.0, 0.0, 0.0])
+        self.rewards = np.array([0.0, 0.0, 0.0])
         self.action_counts = np.array([0, 0, 0])
-        self.current_cumulative_rewards = 0.0
-        self.actions = [1, 2, 3]
+        self.actions = [0, 1, 2]
         self.counts = 0
         self.counts_history = []
+        self.current_cumulative_reward = 0.0
         self.cumulative_rewards_history = []
-        self.a = 1
-        self.reward = 0
-
-    def plot(self, colors, policy):
-        plt.figure(1)
-        plt.plot(self.counts_history, self.cumulative_rewards_history, colors, label=policy)
-        plt.legend()
-        plt.xlabel('n', fontsize=18)
-        plt.ylabel('total rewards', fontsize=18)
-        plt.show()
+        self.current_action = 1
+        self.current_reward = 0.0
 
 
 if __name__ == '__main__':
+    np.random.seed(0)
     k_gamble = KB_Game()
-    total = 2000
-    k_gamble.train(play_total=total, policy='e_greedy', epsilon=0.05)
-    k_gamble.plot(colors='r', policy='e_greedy')
-    k_gamble.reset()
+    play_times = 2000
 
-    k_gamble.train(play_total=total, policy='boltzmann', temperature=1)
-    k_gamble.plot(colors='b', policy='boltzmann')
-    k_gamble.reset()
+    plt.figure(1)
 
-    k_gamble.train(play_total=total, policy='ucb', c_ratio=0.5)
-    k_gamble.plot(colors='g', policy='ucb')
+    k_gamble.train(play_times=play_times, policy='e_greedy', epsilon=0.05)
+    plt.plot(k_gamble.counts_history, k_gamble.cumulative_rewards_history, 'r', label='e_greedy', linestyle='-.')
+    k_gamble.reset()
+    k_gamble.train(play_times=play_times, policy='boltzmann', temperature=1)
+    plt.plot(k_gamble.counts_history, k_gamble.cumulative_rewards_history, 'g', label='boltzmann', linestyle='--')
+    k_gamble.reset()
+    k_gamble.train(play_times=play_times, policy='ucb', c_ratio=0.5)
+    plt.plot(k_gamble.counts_history, k_gamble.cumulative_rewards_history, 'b', label='ucb', linestyle='-')
+
+    plt.legend()
+    plt.xlabel('n', fontsize=18)
+    plt.ylabel('total rewards', fontsize=18)
+    plt.show()
